1d85d40f25309ad20248b81c3aad96d4f5b78f7a
A <ashleyneilbaker@gmail.com>
2025-09-15 12:59:59 +0100

    Update strangerthings_drumandbass.js

--- strangerthings_drumandbass.js
+++ strangerthings_drumandbass.js
@@ -9,9 +9,9 @@ $: s("white!8").decay(tri.range(0.03, 0.1),fast(2)).gain(0.6).almostNever(ply("2
 
 $: s("rim:1").struct(rand.mul(.65)
     .round().seg(16).rib(3,2)).gain(.4)._punchcard( )
-$: s("sine").scrub("0 0 0 0 0 0 0 0")
+$: s("sine").scrub("0 0 0 0 0 0 0 0").gain(1.5)
 
-$: n("0 2 4 6 <7 8?> 6 4 2")
+$: n("0 2 4 6 <7 7?> 6 4 2")
   .scale("<c1:major>/2")
   .s("supersaw")
   .distort(1)
