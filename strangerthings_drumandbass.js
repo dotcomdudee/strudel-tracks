29ab93e5b265f33fa7f157e51b011918b3575c2d
A <ashleyneilbaker@gmail.com>
2025-09-15 13:01:40 +0100

    Update strangerthings_drumandbass.js

--- strangerthings_drumandbass.js
+++ strangerthings_drumandbass.js
@@ -3,7 +3,7 @@ setCps(170/60/4)
 
 $: s("bd:4").beat("0,7?,10",16)
 
-$: s("sd:2").beat("4,12",16).gain(.7)
+$: s("sd:2").beat("4,12",16).gain(.6)
 
 $: s("white!8").decay(tri.range(0.03, 0.1),fast(2)).gain(0.6).almostNever(ply("2"))
 
@@ -17,5 +17,5 @@ $: n("0 2 4 6 <7 7?> 6 4 2")
   .distort(1)
   .superimpose((x) => x.detune("<0.5>"))
   .lpenv(perlin.slow(3).range(1, 4))
-  .lpf(perlin.slow(2).range(100, 200))
+  .lpf(perlin.slow(2).range(100, 600))
   .gain(1)._scope( )
